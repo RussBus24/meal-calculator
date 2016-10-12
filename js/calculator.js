2d6d2db362f0bdc2cee16091a04bbe57fbd9980e
RussBus24 <rjhenry1014@gmail.com>
2016-10-12 16:17:53 -0400

    JS files

---
+++ js/calculator.js
@@ -0,0 +1,49 @@
+var diners = {
+    diner1 : {
+        name: "The Orange & Brew Steakhouse",
+        dish: {
+            name: "New York Prime Rib",
+            price: "$34.95"
+        }
+    },
+    item1: {
+        dish: "New York Prime Rib",
+        price: "$34.95"
+    },
+    item2: {
+        dish: "Roasted Chicken",
+        price: "$17.95"
+    },
+    item3: {
+        dish: "Roasted Chicken",
+        price: "$17.95"
+    }
+}
+
+var obs = {
+    dish: ["New York Prime Rib", "Roasted Chicken", "Red Snapper"],
+    price: ["$34.95", "$17.95", "$24.95"]
+    };
+
+var tsg = {
+    dish: ["BBQ Burger", "Chicken Parmigiana", "Apple Walnut Salad"],
+    price: ["$12.95", "$15.95", "$10.95"]
+    };
+
+var hb = {
+    dish: ["Nachos w/Queso Dip", "Popcorn Shrimp", "Chicken Fingers"],
+    price: ["$9.95", "$10.95", "$11.95"]
+    };
+
+$(document).ready(function() {
+    var Diners = ["The Orange & Brew Steakhouse", "The Singing Gator", "Heisman Bar"];
+    var Dishes = {
+        obs: ["New York Prime Rib", "Roasted Chicken", "Red Snapper"],
+        tsg: ["BBQ Burger", "Chicken Parmigiana", "Apple Walnut Salad"],
+        hb: ["Nachos w/Queso Dip", "Popcorn Shrimp", "Chicken Fingers"]
+    }
+
+    function checkout() {
+
+    };
+});

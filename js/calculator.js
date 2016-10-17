8b4afc0dedb3163e2b2957c3b8f887992d573a82
RussBus24 <rjhenry1014@gmail.com>
2016-10-17 15:23:34 -0400

    Removed some unused code

--- js/calculator.js
+++ js/calculator.js
@@ -101,21 +101,6 @@ var diners = {
     }
 }
 
-var obs = {
-    dish: ["New York Prime Rib", "Roasted Chicken", "Red Snapper"],
-    price: ["$34.95", "$17.95", "$24.95"]
-    };
-
-var tsg = {
-    dish: ["BBQ Burger", "Chicken Parmigiana", "Apple Walnut Salad"],
-    price: ["$12.95", "$15.95", "$10.95"]
-    };
-
-var hb = {
-    dish: ["Nachos w/Queso Dip", "Popcorn Shrimp", "Chicken Fingers"],
-    price: ["$9.95", "$10.95", "$11.95"]
-    };
-
 $(document).ready(function() {
     var Diners = ["The Orange & Brew Steakhouse", "The Singing Gator", "Heisman Bar"];
     var Dishes = {
